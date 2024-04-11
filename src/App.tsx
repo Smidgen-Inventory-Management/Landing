1a4ff7554f7bc128a436143a7bd4a28dd860c65b
JHern603 <jhern603@fiu.edu>
2024-04-11 00:03:21 -0400

    Continuation of components creation

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,50 @@ import Modal from "./components/Modal"
 import "./css/reset.css"
 import "./css/global.css"
 import "./css/components.css"
+import "./css/forms.css"
 
 function App() {
   const [loginModalOpened, setLoginModalOpened] = useState(false);
+  const formContent = () => {
+    return (<form action="" method="GET" className="login-button-container">
+      <TextInput
+        label="Username"
+        disabled={false}
+        withIcon={false}
+      />
+      <TextInput
+        label="Password"
+        disabled={false}
+        withIcon={false}
+        inputType='password'
+      />
+      <div>
+        <button className="button-primary" style={{ marginRight: "20px" }}>Register</button>
+        <button className="button-tertiary">Log In</button>
+      </div>
+    </form>)
+  }
 
   return (
     <div className="page-content">
-      <Navbar state={loginModalOpened} setState={setLoginModalOpened} />
+      <Navbar
+        state={loginModalOpened}
+        setState={setLoginModalOpened}
+      />
+      <Modal
+        state={loginModalOpened}
+        setState={setLoginModalOpened}
+        header="Login to Smidgen"
+        content={formContent()}
+      />
       <div className="content">
-        <Modal
-          state={loginModalOpened}
-          setState={setLoginModalOpened}
-          header="Login to Smidgen"
-          content={<p>Test Content</p>}
-        />
-        <button className="button-primary" disabled={false} >Log In</button>
-        <button className="button-primary" disabled={true} >Log In</button>
-        <button className="button-tertiary" disabled={false} >Log In</button>
-        <button className="button-tertiary" disabled={true} >Log In</button>
-        <button className="button-info" disabled={false} >Log In</button>
-        <button className="button-info" disabled={true} >Log In</button>
-        <button className="button-destructive" disabled={false} >Log In</button>
-        <button className="button-destructive" disabled={true} >Log In</button>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <h2>Lorem ipsum dolor sit amet.</h2>
-        <h3>Lorem ipsum dolor sit amet.</h3>
-        <h4>Lorem ipsum dolor sit amet.</h4>
-        <h5>Lorem ipsum dolor sit amet.</h5>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis inventore animi, aliquam dolorem est, facilis nam accusantium eius eveniet nisi sunt voluptatem impedit illum ipsam in ratione vero itaque rem praesentium deleniti eum commodi culpa. Sed modi voluptates molestias, animi, ea asperiores nesciunt consequuntur quaerat excepturi reiciendis consequatur delectus maiores repellendus. Optio enim culpa quaerat accusantium natus, harum ipsa dolor minima recusandae laboriosam hic at nisi labore suscipit cumque cum est, explicabo necessitatibus animi quia officia, quod quo assumenda? Porro autem officiis nobis tempore similique doloribus pariatur possimus molestias maiores quaerat? Amet blanditiis expedita quo porro autem magnam reprehenderit veniam.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus explicabo, provident excepturi laborum consequatur quis temporibus ducimus dolore corporis atque dolorum voluptatibus deserunt vel nostrum esse aperiam amet totam assumenda autem et. Nam eius mollitia id consectetur rerum doloribus ducimus!</p>
+        <button className="button-primary" disabled={false} >Primary Button</button>
+        <button className="button-tertiary" disabled={false} >Tertiary Button</button>
+        <button className="button-info" disabled={false} >Info Button</button>
+        <button className="button-destructive" disabled={false} >Destructive Buton</button>
 
-        <form action="" method="get">
-          <label className="inline-label"><input type="radio" name="radio" />Radio Button</label>
-          <label className="inline-label"><input type="radio" name="radio" />Radio Button</label>
-          <label className="inline-label"><input type="checkbox" name="checkbox" className="checkbox" /> Checkbox</label>
-          <label className="inline-label"><input type="checkbox" name="checkbox" className="checkbox" /> Checkbox</label>
-          <input type="checkbox" name="checkbox" className="toggle" />
-          <TextInput />
-        </form>
+        <input type="checkbox" className='toggle' />
+        
         <h2>Need to edit Select</h2>
         <select>
           <option>The quick brown fox jumps over the lazy dog</option>
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,12 +1,26 @@
 import "../css/components.css"
 
-export default function TextInput() {
+
+interface TextInputProps {
+  type?: string;
+  disabled: boolean;
+  placeholder?: string;
+  rightAlignedIcon?: boolean;
+  label: string;
+  supportiveText?: string;
+  withIcon: boolean;
+  inputType?: string;
+}
+
+const TextInput: React.FC<TextInputProps> = ({ inputType="text", withIcon, type, disabled, placeholder, rightAlignedIcon, label, supportiveText }) => {
   return (
     <div className="text-input-container">
-      <h4 className="text-input-label">Label</h4>
-      <input disabled={false} className="error text-input" type="text" placeholder="Input label" />
-      <div className="foot-error icon-right" />
-      <p className="text-input-subtitle">Supportive Text</p>
+      <h4 className="text-input-label">{label}</h4>
+      <input disabled={disabled} className={`text-input ${type}`} type={inputType} placeholder={placeholder} />
+      {withIcon ? <div className={`foot-${type} ${rightAlignedIcon ? 'icon-right' : ''}`} /> : ''}
+      <p className="text-input-subtitle">{supportiveText}</p>
     </div>
   )
 }
+
+export default TextInput;
