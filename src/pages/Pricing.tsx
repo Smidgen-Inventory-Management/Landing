590b49d37c4d09a8cab6ebdba13caea461621fa4
JHern603 <jhern603@fiu.edu>
2024-06-11 23:43:38 -0400

    Basic table styling

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,3 +1,5 @@
+import '../css/tables.css';
+
 const Pricing = () => {
   return (
     <div className="body">
@@ -6,44 +8,53 @@ const Pricing = () => {
           <table className="pt">
             <thead className="pt-header-container">
               <tr className="pt-header-row">
-                <th className="pt-header-item" >(All Prices Shown in USD)</th>
-                <th className="pt-header-item" >Self Hosted</th>
-                <th className="pt-header-item" >Small Business</th>
-                <th className="pt-header-item" >Enterprise</th>
+                <th className="pt-header-item table-grayed">
+                  <h6>(All Prices Shown in USD)</h6>
+                </th>
+                <th className="pt-header-item">
+                  <h5>Self Hosted</h5>
+                </th>
+                <th className="pt-header-item">
+                  <h5>Small Business</h5>
+                </th>
+                <th className="pt-header-item">
+                  <h5>Enterprise</h5>
+                </th>
               </tr>
             </thead>
             <tbody className="pt-body-container">
               <tr className="pt-body-row">
                 <td className="pt-body-row-data">
-                  <p>Billed Annualy</p>
-                  <p>Billed Monthly</p>
+                  <h5>Billed Annualy</h5>
+                  <h6>Billed Monthly</h6>
                 </td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
+                <td className="pt-body-row-data">$0</td>
+                <td className="pt-body-row-data">$30</td>
+                <td className="pt-body-row-data">Specal Pricing</td>
               </tr>
               <tr className="pt-body-row">
                 <td className="pt-body-row-data">
-                  <p>Max Users</p>
+                  <h5>Max Users</h5>
                 </td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
+                <td className="pt-body-row-data">$0</td>
+                <td className="pt-body-row-data">$30</td>
+                <td className="pt-body-row-data">Specal Pricing</td>
               </tr>
               <tr className="pt-body-row">
                 <td className="pt-body-row-data">
-                  <p>Max Asssets</p>
+                  <h5>Max Asssets</h5>
                 </td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
+                <td className="pt-body-row-data">$0</td>
+                <td className="pt-body-row-data">$30</td>
+                <td className="pt-body-row-data">Specal Pricing</td>
               </tr>
               <tr className="pt-body-row">
                 <td className="pt-body-row-data">
-                  <p>Community Support</p>
+                  <h5>Community Support</h5>
                 </td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
-                <td className="pt-body-row-data"></td>
+                <td className="pt-body-row-data">$0</td>
+                <td className="pt-body-row-data">$30</td>
+                <td className="pt-body-row-data">Specal Pricing</td>
               </tr>
             </tbody>
           </table>
